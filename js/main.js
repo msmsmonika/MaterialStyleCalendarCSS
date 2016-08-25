ea802ebfe62b29cbbbcaf9983e22add845a1426e
msmsmonika <msms.monika@gmail.com>
2016-08-25 23:05:38 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+    $('#datetimepicker12').datepicker({
+        dateFormat: 'yyyy/mm/dd',
+        inline: true,
+        sideBySide: true,
+        onSelect: function (date) {
+            console.log(date);
+        }
+    });
+   
+});
+
